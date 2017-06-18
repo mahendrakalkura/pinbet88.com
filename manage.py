afb4a939ba2aa8c3e00766400fd7eccb04b001e7
Mahendra Kalkura <mahendra@kalkura.com>
2017-06-18 14:38:45 +0530

    manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
 from datetime import date, datetime
-from os import getenv
 from pprint import pprint
 from sys import argv
 from time import time
 
-from requests import Session
-from requests.cookies import RequestsCookieJar
+from requests import request
 
 
 def main(options):
@@ -15,32 +13,12 @@ def main(options):
 
 
 def execute_matches():
-    session = Session()
-    response = home(session)
-    response = sign_in(session)
-    matches = get_matches(session)
+    matches = get_matches()
     pprint(matches)
 
 
-def home(session):
-    url = 'https://www.pinbet88.com/en/'
-    method = 'GET'
-    response = session.request(url=url, method=method)
-    return response
-
-
-def sign_in(session):
-    url = 'https://www.pinbet88.com/member-service/v1/login?locale=en_US'
-    method = 'POST'
-    data = {
-        'loginId': getenv('USERNAME'),
-        'password': getenv('PASSWORD'),
-    }
-    response = session.request(url=url, method=method, data=data)
-    return response
-
-
-def get_matches(session):
+def get_matches():
+    matches = []
     url = 'https://ropinwev.pinbet88.com/sports-service/sv/odds/events'
     method = 'GET'
     _ = time()
@@ -65,13 +43,8 @@ def get_matches(session):
         'tm': '0',
         'v': '0',
     }
-    headers = {
-        'host': 'ropinwev.pinbet88.com',
-        'referer': 'https://ropinwev.pinbet88.com/en/sports',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/5.0 (KHTML, like Gecko) Chrome/5.0 Safari/5.0',
-        'x-requested-with': 'XMLHttpRequest',
-    }
-    response = session.request(url=url, method=method, params=params)
+    response = request(url=url, method=method, params=params)
+    print(response.text)
     json = response.json()
     tournaments = json['l'][0][2] + json['n'][0][2]
     for tournament in tournaments:
