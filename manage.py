77c9150673a95a7224138741a8654fa66f93063d
Mahendra Kalkura <mahendra@kalkura.com>
2017-07-15 21:00:19 +0530

    manage.py

--- manage.py
+++ manage.py
@@ -1,20 +1,24 @@
-from contextlib import closing
 from datetime import date, datetime
-from pprint import pprint
 from os.path import devnull
+from pprint import pprint
 from sys import argv
 from time import time
 from traceback import print_exc
 
 from requests import request
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.remote_connection import RemoteConnection
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
-TIMEOUT = 30
 URLS_HOME = 'https://www.pinbet88.com/en/sports/soccer'
 URLS_MATCHES = 'https://www.pinbet88.com/sports-service/sv/odds/events'
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/5.0 (KHTML, like Gecko) Chrome/5.0 Safari/5.0'
+URLS_MATCH = 'http://live.pinbet88.com/live-center-service/s/match/restore/{id:s}'
+USERNAME = 'CA701YT003'
+PASSWORD = 'abcd1234'
+TIMEOUT = 30
+USER_AGENT = 'Mozilla/6.0 (X11; Linux x86_64) AppleWebKit/6.0 (KHTML, like Gecko) Chrome/6.0 Safari/6.0'
 
 RemoteConnection._timeout = TIMEOUT
 
@@ -23,19 +27,22 @@ def main(options):
     if options[1] == '--matches':
         execute_matches()
         return
+    if options[1] == '--match':
+        execute_match(options[2])
+        return
 
 
 def execute_matches():
-    matches = get_matches()
+    firefox_profile = get_firefox_profile()
+    browser = get_browser(firefox_profile)
+    matches = get_matches(browser)
+    browser.quit()
     pprint(matches)
 
 
-def get_matches():
+def get_matches(browser):
     matches = []
-    firefox_profile = get_firefox_profile()
-    browser = webdriver.Firefox(firefox_profile=firefox_profile, log_path=devnull)
-    with closing(browser) as browser:
-        contents, cookies = get_contents_and_cookies(browser)
+    contents, cookies = get_matches_contents_and_cookies(browser)
     if not contents:
         return matches
     json = None
@@ -72,7 +79,7 @@ def get_matches():
         if response:
             json = response.json()
     except Exception:
-        print_exc()
+        pass
     if not json:
         return matches
     tournaments = []
@@ -102,6 +109,109 @@ def get_matches():
     return matches
 
 
+def get_matches_contents_and_cookies(browser):
+    contents = None
+    try:
+        browser.get(URLS_HOME)
+        wait = WebDriverWait(browser, TIMEOUT)
+        wait.until(get_matches_condition)
+        contents = browser.execute_script('return document.getElementsByTagName("html")[0].innerHTML')
+    except Exception:
+        pass
+    print(browser.page_source)
+    exit()
+    cookies = browser.get_cookies()
+    cookies = {cookie['name']: cookie['value'] for cookie in cookies}
+    return contents, cookies
+
+
+def get_matches_condition(browser):
+    status = expected_conditions.presence_of_element_located((By.ID, 'sports'))
+    if not status:
+        return False
+    return True
+
+
+def execute_match(id):
+    firefox_profile = get_firefox_profile()
+    browser = get_browser(firefox_profile)
+    match = get_match(browser, id)
+    browser.quit()
+    pprint(match)
+
+
+def get_match(browser, id):
+    match = []
+    contents, cookies = get_match_contents_and_cookies(browser)
+    pprint(cookies)
+    return match
+    if not contents:
+        return match
+    json = None
+    try:
+        method = 'GET'
+        url = URLS_MATCH
+        url = url.format(id=id)
+        headers = {
+            'referer': 'https://www.pinbet88.com/en/sports/soccer',
+            'user-agent': USER_AGENT,
+            'x-requested-with': 'XMLHttpRequest',
+        }
+        response = request(method, url, cookies=cookies, headers=headers, timeout=TIMEOUT)
+        response.raise_for_status()
+        json = response.json()
+    except Exception:
+        print_exc()
+    if not json:
+        return match
+    match = json
+    return match
+
+
+def get_match_contents_and_cookies(browser):
+    contents = None
+    try:
+        browser.get(URLS_HOME)
+        wait = WebDriverWait(browser, TIMEOUT)
+        wait.until(get_match_condition_1)
+        username = browser.find_element_by_name('loginId')
+        username.send_keys(USERNAME)
+        password = browser.find_element_by_name('password')
+        password.send_keys(PASSWORD)
+        button = browser.find_element_by_id('login')
+        button.click()
+        wait = WebDriverWait(browser, TIMEOUT)
+        wait.until(get_match_condition_2)
+    except Exception:
+        print_exc()
+    print(browser.page_source)
+    exit()
+    cookies = browser.get_cookies()
+    cookies = {cookie['name']: cookie['value'] for cookie in cookies}
+    return contents, cookies
+
+
+def get_match_condition_1(browser):
+    status = expected_conditions.presence_of_element_located((By.NAME, 'loginId'))
+    if not status:
+        return False
+    status = expected_conditions.presence_of_element_located((By.NAME, 'password'))
+    if not status:
+        return False
+    return True
+
+
+def get_match_condition_2(browser):
+    if 'Welcome Back' in browser.page_source:
+        return True
+    return False
+
+
+def get_browser(firefox_profile):
+    browser = webdriver.Firefox(firefox_profile=firefox_profile, log_path=devnull)
+    return browser
+
+
 def get_firefox_profile():
     firefox_profile = webdriver.FirefoxProfile()
     firefox_profile.add_extension('adblock_plus.xpi')
@@ -109,35 +219,6 @@ def get_firefox_profile():
     firefox_profile.set_preference('network.proxy.type', 0)
     firefox_profile.set_preference('xpinstall.signatures.required', False)
     return firefox_profile
-
-
-def conditions_home(browser):
-    if 'id="sports"' in browser.page_source:
-        return True
-    return False
-
-
-def get_contents_and_cookies(browser):
-    contents = None
-    try:
-        browser.get(URLS_HOME)
-        wait = WebDriverWait(browser, TIMEOUT)
-        wait.until(conditions_home)
-        contents = browser.execute_script('return document.getElementsByTagName("html")[0].innerHTML')
-    except Exception:
-        print_exc()
-    cookies = browser.get_cookies()
-    cookies = {cookie['name']: cookie['value'] for cookie in cookies}
-    return contents, cookies
-
-
-def conditions_matches(browser):
-    print(browser.page_source[-100:])
-    if browser.page_source.startswith('{'):
-        print('conditions_matches - True')
-        return True
-    print('conditions_matches - False')
-    return False
 
 
 if __name__ == '__main__':
